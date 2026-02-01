a4a29c1e818738d2ca425e2e006648d90c9b7257
ssl-cloud <sslcloudlearning@gmail.com>
2026-02-01 23:50:47 +0530

    Demo-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>🏦 Welcome to Secure Bank</h1>
+      <p>Your money is safe with us.</p>
+
+      <div className="card">
+        <h3>Account Balance</h3>
+        <p>₹ 1,25,000</p>
+      </div>
+
+      <button>Transfer Money</button>
+    </div>
+  );
+}
+
+export default App;
